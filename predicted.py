e4cc9a660ba3107160994cccde0c04a97be6bf3d
MuHe <1652080231@qq.com>
2024-03-13 08:49:16 +0800

    将人工标注与模型标注的图片放在左右两边拼接而成

--- predicted.py
+++ predicted.py
@@ -1,6 +1,7 @@
 import os
 import cv2
 import numpy as np
+from tqdm import tqdm
 
 
 def layer_overlay(source_images, source_labels, output_dir):
@@ -70,11 +71,50 @@ def draw_outline(source_images, source_labels, output_dir):
 
 
 if __name__ == "__main__":
-    source_images = "auxiliary/task4/img"
-    source_labels = "auxiliary/task4/label"
-    output_dir = "predicted/res"
-
+    source_images = "dataset/test/images"
+    source_true_labels = "dataset/test/labels"
+    source_pred_labels = "dataset/test_pred"
+    output_dir = "pred"
 
     # layer_overlay(source_images, source_labels, output_dir)
-    draw_outline(source_images, source_labels, output_dir)
+    # draw_outline(source_images, source_labels, output_dir)
+
+    os.makedirs(output_dir, exist_ok=True)
+
+    files = [file for file in os.listdir(source_images) if file.endswith(".png")]
+
+    for file in tqdm(files, desc="Processing", unit="files"):
+        image_path = os.path.join(source_images, file)
+        true_label_path = os.path.join(source_true_labels, file)
+        true_label = cv2.imread(true_label_path, cv2.IMREAD_GRAYSCALE)
+        pred_label_path = os.path.join(source_pred_labels, file)
+        pred_label = cv2.imread(pred_label_path, cv2.IMREAD_GRAYSCALE)
+
+        image1 = cv2.imread(image_path)
+        label1 = np.zeros_like(true_label)
+        label1[true_label == 1] = 255
+        contours, hierarchy = cv2.findContours(label1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        cv2.drawContours(image1, contours, -1, (0, 255, 0), thickness=2)
+        cv2.imwrite("{}/{}".format(output_dir, file), image1)
+        label2 = np.zeros_like(true_label)
+        label2[true_label == 2] = 255
+        contours, hierarchy = cv2.findContours(label2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        cv2.drawContours(image1, contours, -1, (255, 0, 0), thickness=2)
+
+        image2 = cv2.imread(image_path)
+        label1 = np.zeros_like(pred_label)
+        label1[pred_label == 1] = 255
+        contours, hierarchy = cv2.findContours(label1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        cv2.drawContours(image2, contours, -1, (0, 255, 0), thickness=2)
+        cv2.imwrite("{}/{}".format(output_dir, file), image2)
+        label2 = np.zeros_like(pred_label)
+        label2[pred_label == 2] = 255
+        contours, hierarchy = cv2.findContours(label2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        cv2.drawContours(image2, contours, -1, (255, 0, 0), thickness=2)
+
+        # 将两张图片拼接在一起
+        merged_image = cv2.hconcat([image1, image2])
+
+        # 保存拼接后的图片
+        cv2.imwrite("{}/{}".format(output_dir, file), merged_image)
 
