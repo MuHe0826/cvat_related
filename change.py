6c6584538c7572d4c5bf3788712d42f664b85f28
MuHE <93702142+MuHe0826@users.noreply.github.com>
2024-04-02 08:51:22 +0800

    修改了一部分代码

--- change.py
+++ change.py
@@ -3,14 +3,15 @@ import xml.dom.minidom as xmldom
 
 if __name__ == "__main__":
     # 加载 XML 文件
-    xml_file = xmldom.parse('3.xml')
+    xml_file = xmldom.parse('annotations.xml')
     tracks = xml_file.getElementsByTagName('track')
 
     # 遍历所有的标注对象
     for track in tracks:
         label = track.getAttribute('label')
-        if label == 'cotton_ball':
-            track.setAttribute('label', 'gauze')
+        if label == 'right_angle_grab':
+            track.setAttribute('label', 'forceps')
+            print(1)
 
     # 将修改后的 XML 文件保存
     with open('modified_annotations.xml', 'w') as f:
