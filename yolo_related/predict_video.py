a5595f10e09cc25066f297b75e91c620081986b1
1jozon <71960255+1jozon@users.noreply.github.com>
2024-04-24 08:53:21 +0800

    Update predict_video.py

--- yolo_related/predict_video.py
+++ yolo_related/predict_video.py
@@ -1,11 +1,11 @@
 import cv2
 from ultralytics import YOLO
-
+import time
 # Load the YOLOv8 model
-model = YOLO('../best.pt')
+model = YOLO(r'C:\Users\jozon\Downloads\yolo\v8n\best02.pt')
 
 # Open the video file
-video_path = r"../../video/M_05202021095646_0U53469052015646_1_002_009-1modify_fps_rate.mp4"
+video_path = r"D:\pycharm\pythonProject1\utils\video\M_10102022034410_0000000015636581_2_001_0007-01modify_fps_rate.mp4"
 cap = cv2.VideoCapture(video_path)
 
 # Loop through the video frames
@@ -13,12 +13,17 @@ while cap.isOpened():
     # Read a frame from the video
     success, frame = cap.read()
 
-    if success:
-        # Run YOLOv8 inference on the frame
-        results = model(frame)
 
+    if success:
+        start = time.perf_counter()
+        # Run YOLOv8 inference on the frame
+        results = model(frame,device='cuda',show_boxes=False,)
+        end = time.perf_counter()
+        total = end - start
+        fps = 1 / (end - start)
         # Visualize the results on the frame
         annotated_frame = results[0].plot()
+        cv2.putText(annotated_frame,f"FPS: {int(fps)}",(0, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
         cv2.namedWindow('YOLOv8 Inference', cv2.WINDOW_NORMAL)
         # Display the annotated frame
         cv2.imshow("YOLOv8 Inference", annotated_frame)
